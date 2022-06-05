c4eace19f9d6870f41f455b9018dc6d4066f3f18
IceCreamCracker <matheus9bernard@hotmail.com>
2022-06-05 16:10:48 -0400

    virtualenv,flask,api,ajax

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import sys
 sys.path.insert(0,"./src")
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
+CORS(app)
 @app.route('/')
 def home():
-    return render_template("home.html")
+    lang = request.args.get('lang')
+    return render_template("home.html", lang=lang)
+
+@app.route('/api')
+def api():
+    lista = {}
+    lista["x1"] = request.args.get('x1')
+    lista["y1"] = request.args.get('y1')
+    lista["x2"] = request.args.get('x2')
+    lista["y2"] = request.args.get('y2')
+
+    return make_response(jsonify(lista))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
--- static/js/template.js
+++ static/js/template.js
@@ -1,24 +1,50 @@
+
 //functions
-fodase = () => {
-    alert("Hello World diferenciado")
-}
+
+//canvas management
 drawRect = (x1,y1,x2,y2) => {
     canvas = document.getElementById("canvas")
     ctx = canvas.getContext("2d")
-    ctx.lineWidth = 1
     ctx.beginPath()
+    ctx.strokeRect(0,0,20,20)
+
+    ctx.lineWidth = 1
     ctx.strokeRect(x1,y1,Math.abs(x1-x2),Math.abs(y1-y2))
-    ctx.stroke()
 }
 clearAll = () => {
     canvas = document.getElementById("canvas")
     ctx = canvas.getContext("2d")
     ctx.clearRect(0,0,canvas.width,canvas.height)
 }    
-updateCanvas(rects => {
+updateCanvas = (rects => {
     clearAll()
     rects.map(a => {
         drawRect(a[0],a[1],a[2],a[3])
     })
 })
 
+//API REQUESTS
+reqRect = (x1,y1,x2,y2) => {
+    $.ajax({
+        url:"http://127.0.0.1:5000/api",
+        method:"get",
+        data: {
+            x1:x1,
+            y1:y1,
+            x2:x2,
+            y2:y2
+        },
+
+        success: (res)=>{
+            $("#x1").html(`${res["x1"]}`)
+            $("#x2").html(`${res["x2"]}`)
+            $("#y1").html(`${res["y1"]}`)
+            $("#y2").html(`${res["y2"]}`)
+            
+        }
+    })
+        // .done((res)=>{
+        // console.log("foi")
+        // console.log(res)
+    // })
+}
